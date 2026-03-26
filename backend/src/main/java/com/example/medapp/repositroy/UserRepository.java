27edc0af930ac48f38eb7542857ffde08cc45f2e
itok12 <142388216+itok12@users.noreply.github.com>
2026-03-26 20:27:51 +0000

    Delete backend/src/main/java/com/example/medapp/repositroy/UserRepository.java

--- backend/src/main/java/com/example/medapp/repositroy/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.medapp.repository;
-
-import com.example.medapp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
